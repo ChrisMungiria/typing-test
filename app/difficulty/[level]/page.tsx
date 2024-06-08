2ed933467401b15e46a32c8e28c5d14622b1ed12
Chris Mungiria <christopher.mungiria@strathmore.edu>
2024-06-09 00:16:42 +0300

    feat: added an input field for user ipnut

--- app/difficulty/[level]/page.tsx
+++ app/difficulty/[level]/page.tsx
@@ -1,29 +1,56 @@
-import React from "react";
+"use client";
 
-const DifficultyLevel = async ({ params }: { params: { level: string } }) => {
-  // const words = await fetch(`https://.../${params.level}`).then((res) => res.json());
-  const words = [
-    "bicycle",
-    "elephant",
-    "balloon",
-    "theater",
-    "adventure",
-    "mystery",
-    "diamond",
-    "courage",
-    "kitchen",
-    "journal",
-    "whisper",
-    "library",
-    "lantern",
-    "festival",
-    "journey",
-    "capture",
-    "garden",
-    "pattern",
-    "tractor",
-    "blanket",
-  ];
+import { useEffect, useRef, useState } from "react";
+
+const paragraph =
+  "Sunlight filtered through the dense forest canopy, casting dappled shadows on the mossy ground. Birds chirped melodiously, while a gentle breeze rustled the leaves. Nearby, a babbling brook provided tranquil background music.";
+
+const DifficultyLevel = ({ params }: { params: { level: string } }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const charRefs = useRef<HTMLSpanElement[]>([]);
+
+  const [charIndex, setCharIndex] = useState<number>(0);
+  const [isTyping, setIsTyping] = useState<boolean>(false);
+
+  const [correctWrong, setCorrectWrong] = useState<string[]>([]);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+    setCorrectWrong(Array(charRefs.current.length).fill(""));
+  }, []);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const characters = charRefs.current;
+    let currentChar = charRefs.current[charIndex];
+    let typedChar = e.target.value.slice(-1);
+
+    if (charIndex < characters.length) {
+      if (!isTyping) {
+        setIsTyping(true);
+      }
+
+      if (typedChar === currentChar.textContent) {
+        setCharIndex(charIndex + 1);
+        setCorrectWrong((prevCorrectWrong) => [
+          ...prevCorrectWrong.slice(0, charIndex),
+          "correct",
+        ]);
+      } else {
+        setCharIndex(charIndex + 1);
+        // Add a mistake state here
+        setCorrectWrong((prevCorrectWrong) => [
+          ...prevCorrectWrong.slice(0, charIndex),
+          "wrong",
+        ]);
+      }
+      if (charIndex === characters.length - 1) {
+        setIsTyping(false);
+        // paragraph complete
+      }
+    } else {
+      setIsTyping(false);
+    }
+  };
   return (
     <>
       <h1 className="text-2xl tracking-widest uppercase  font-light">
@@ -33,17 +60,28 @@ const DifficultyLevel = async ({ params }: { params: { level: string } }) => {
       {/* Timer */}
       <p className="text-7xl my-3 font-light tracking-wide">00:00</p>
 
-      {/* Words */}
-
-      <div className="w-11/12 max-w-3xl flex flex-wrap items-center justify-center gap-2 text-[#5E6572]">
-        {words.map((word, index) => (
-          <p
+      {/* Paragraph */}
+      <div className="select-none text-center cursor-text">
+        <input
+          type="text"
+          ref={inputRef}
+          className="opacity-0 z-[-999] absolute"
+          onChange={handleChange}
+        />
+        {paragraph.split("").map((char, index) => (
+          <span
+            className={`text-slate-500 ${
+              index === charIndex ? "bg-slate-100/50" : ""
+            }
+      
+      ${correctWrong[index] === "wrong" && " text-custom-red"} 
+      ${correctWrong[index] === "correct" && "text-custom-green"}
+     `}
             key={index}
-            className="text-center rexrlg
-          "
+            ref={(e: any) => (charRefs.current[index] = e)}
           >
-            {word}
-          </p>
+            {char}
+          </span>
         ))}
       </div>
     </>
